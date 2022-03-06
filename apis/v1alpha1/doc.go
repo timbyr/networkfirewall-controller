c88699b36c42139a24b5ed0180d1996e3c23339f
Keith Byrne <timbyr@gmail.com>
2022-03-06 22:21:11 +0000

    Initial

---
+++ apis/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// +k8s:deepcopy-gen=package
+// Package v1alpha1 is the v1alpha1 version of the networkfirewall.services.k8s.aws API.
+// +groupName=networkfirewall.services.k8s.aws
+package v1alpha1
